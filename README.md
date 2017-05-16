9763ed4a067fc3d3150e93c16d391507ec23fa11
Daan Raman <draman@nviso.be>
2017-05-16 18:14:09 +0200

    commit message

---
+++ README.md
@@ -0,0 +1,107 @@
+# binsnitch.py
+binsnitch can be used to detect silent unwanted changes to files on your system.
+It will scan a given directory recursively for files and keep track of any changes it detects, based on the SHA256 hash of the file.
+You have the option to either track executable files, or all files.
+
+### Requirements
+- python 3
+
+### Running and usage
+```
+usage: binsnitch.py [-h] [-v] [-s] [-a] [-n] [-b] [-w] dir
+
+positional arguments:
+  dir               the directory to monitor
+
+optional arguments:
+  -h, --help        show this help message and exit
+  -v, --verbose     increase output verbosity
+  -s, --singlepass  do a single pass over all files
+  -a, --all         keep track of all files, not only executables
+  -n, --new         alert on new files too, not only on modified files
+  -b, --baseline    do not generate alerts (useful to create baseline)
+  -w, --wipe        start with a clean db.json and alerts.log file
+```
+
+Example: monitor all executable files on the system and enable verbose logging
+
+```
+python3.5 binsnitch.py -v / 
+```
+
+Example: monitor all files in the current directory and enable verbose logging
+
+```
+python3.5 binsnitch.py -v -a . 
+```
+
+### How it works
+Once ``binsnitch.py`` is running, it will scan all files in ``dir`` (provided through a required command line argument) recursively, and create a SHA256 hash of each file it finds. It then does the following:
+- If a file is not known yet by ``binsnitch.py``, its details will be added to ``data/db.json`` (file name, file type and hash).
+- If a file is already known but the calculated hash is different from the one in ``data/db.json``, an alert will be logged to ``data/alert.log``. In addition, the new hash will be added to the appropriate entry in ``data/db.json``.
+- If a file is already known and the hash is identical to the one already in ``data/alert.log``, nothing happens.
+
+### Example output
+
+##### data/alerts.log
+```
+05/15/2017 02:46:17 AM - INFO - Scanning system for new and modified files, this can take a long time
+05/15/2017 02:53:38 AM - INFO - Modified file detected:/Applications/Cyberduck.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app/Contents/MacOS/Autoupdate - new hash: a897613ab9ecd8ead7b697012036b2ef683a9df7afe99d9013e5dd6c3e08af10
+05/15/2017 02:53:39 AM - INFO - Modified file detected:/Applications/Cyberduck.app/Contents/Frameworks/Sparkle.framework/Versions/A/Resources/Autoupdate.app/Contents/MacOS/fileop - new hash: cdad8d7b1cce37547223a198e9fbbe256aed3919b58e1b2305870aeaac33c966
+05/15/2017 02:53:41 AM - INFO - Modified file detected:/Applications/Cyberduck.app/Contents/MacOS/Cyberduck - new hash: 3941de0b9001c616c6fcfdb76108fa5da46bdcdd3089e1feb65578c2d251eeec
+```
+
+##### data/db.json
+
+```
+[
+    {
+        "path": "/Applications/1Password 6.app/Contents/Frameworks/AgileLibrary.framework/Versions/A/Resources/pngquant",
+        "sha256": [
+            "47ecd7d9978a291de70aaf5e4392664d5c697cd0867bb59f3d6833671b83d448"
+        ],
+        "type": "Mach-O 64-bit executable x86_64"
+    }
+]
+```
+
+### Internals
+Checking if a file is executable is done by checking it against a fixed list of dangerous file extensions (check ``binsnitch.py`` source for details).
+
+In its current version, ``binsnitch.py`` eats up a lot of CPU. This is caused by the recursive walk through the filesystem and the calculation of SHA256 hashes for each and every file it encounters.
+
+### Ideas for improvement
+
+- ~~Include a switch to start with a new alerts and db file upon start~~ ☑
+- ~~Include a switch to also process new files~~ ☑
+- ~~Enable a switch to process all files instead of executables only~~ ☑
+- ~~Include a switch for a single pass instead of running forever~~ ☑
+- ~~Remove dependency on ``file`` command to check for file type information~~ ☑
+- Be nicer to system resources (IO and CPU)
+
+### Why binsnitch?
+
+Malware will often settle itself by overwriting existing executable applications in order to avoid detection.
+Recent malware cases (May 2017) do this, including HandBrake being hacked to drop new variant of the Proton malware and the WannaCry ransomware overwriting ``C:\WINDOWS\system32\tasksche.exe``.
+This triggered us to write a simple tool that could be used to detect this.
+
+binsnitch can also be used during malware analysis, to detect silent changes to files (i.e. replacement of a trusted Windows executable by a trojaned version).
+
+### References
+
+Similar tools:
+- Microsoft File Checksum Integrity Verifier - https://www.microsoft.com/en-us/download/details.aspx?id=11533
+- Syscheck in OSSEC - http://ossec-docs.readthedocs.io/en/latest/manual/syscheck/
+
+These tools are either OS-dependent or require installation of libraries. 
+
+``binsnitch.py`` aims at being dependent on core packages available in ``python`` only.
+
+### Community
+
+Bug reports and feature requests are welcome in the issues tab!
+
+Contact us: research@nviso.be.
+
+binsnitch is developed and maintained by Daan Raman ([@daanraman](https://twitter.com/daanraman)).
+
---
+++ binsnitch.py
@@ -0,0 +1,216 @@
+import subprocess
+import time
+import argparse
+import hashlib
+import json
+import logging
+import os
+import signal
+import traceback
+
+# Status Constants
+import sys
+
+FILE_KNOWN_UNTOUCHED = "FILE_KNOWN_UNTOUCHED"
+FILE_KNOWN_TOUCHED = "FILE_KNOWN_TOUCHED"
+FILE_UNKNOWN = "FILE_UNKNOWN"
+
+# List of dangerous file extensions
+dangerous_extensions = set(["ACTION","APK","APP","BAT","BIN","CMD","COM","COMMAND","CPL","CSH","EXE","GADGET","INF1","INS","INX","IPA","ISU","JOB","JSE","KSH","LNK","MSC","MSI","MSP","MST","OSX","OUT","PAF","PIF","PRG","PS1","REG","RGS","RUN","SCT","SH","SHB","SHS","U3P","VB","VBE","VBS","VBSCRIPT","WORKFLOW","WS","WSF"])
+
+# Global variables
+cached_db = None
+
+###########
+#Utilities#
+###########
+def shellquote(s):
+    return "'" + s.replace("'", "'\\''") + "'"
+
+
+def sha256_checksum(filename, block_size=65536):
+    sha256 = hashlib.sha256()
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(block_size), b''):
+            sha256.update(block)
+    return sha256.hexdigest()
+
+
+def check_file_status(file_info):
+    global cached_db
+
+    known_path = False
+
+    for db_file in cached_db:
+        if db_file["path"] == file_info["path"]:
+            known_path = True
+
+            if file_info["sha256"] in db_file["sha256"]:
+                return FILE_KNOWN_UNTOUCHED
+            else:
+                return FILE_KNOWN_TOUCHED
+
+    if not known_path:
+        return FILE_UNKNOWN
+
+
+def add_alert_do_db(file_info, status):
+    global cached_db
+
+    with open("binsnitch_data/db.json") as data_file:
+        db_data = json.load(data_file)
+
+    for db_file in db_data:
+        if db_file["path"] == file_info["path"]:
+            if file_info["sha256"] not in db_file["sha256"]:
+                db_file["sha256"].append(file_info["sha256"])
+
+            if status == FILE_UNKNOWN:
+                logging.info("New file detected:" + db_file["path"] + " - hash: " + file_info["sha256"])
+
+            if status == FILE_KNOWN_TOUCHED:
+                logging.info("Modified file detected:" + db_file["path"] + " - new hash: " + file_info["sha256"])
+
+    cached_db = db_data
+    write_to_db(cached_db)
+
+
+def write_to_db(db_data):
+    s = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    json.dump(db_data, open("binsnitch_data/db.json", 'w'), sort_keys=False, indent=4, separators=(',', ': '))
+    signal.signal(signal.SIGINT, s)
+
+
+def add_file_to_db(file_info):
+    global cached_db
+
+    with open("binsnitch_data/db.json") as data_file:
+        db_data = json.load(data_file)
+
+    file_info_to_add = {"path": file_info["path"], "sha256": [file_info["sha256"]]}
+
+    db_data.append(file_info_to_add)
+    cached_db = db_data
+    write_to_db(cached_db)
+
+
+def refresh_cache():
+    global cached_db
+    try:
+        file = open("binsnitch_data/db.json", 'r')
+        cached_db = json.load(file)
+    except Exception as exc:
+        print(str(sys.exc_info()))
+
+
+def prepare_data_files(args):
+    global cached_db
+
+    # Wipe both alerts and db file in case the user wants to start fresh
+    try:
+        if args.wipe:
+            os.remove("binsnitch_data/db.json")
+            os.remove("binsnitch_data/alerts.log")
+    except IOError:
+        pass # if the files are not there yet, then the wipe does not do anything anyway
+
+    # Make sure the data folders exist
+    if not os.path.exists("./binsnitch_data"):
+        os.makedirs("./binsnitch_data")
+
+    try:
+        file = open("binsnitch_data/db.json", 'r')
+    except IOError:
+        json.dump([], open("binsnitch_data/db.json", 'w'))
+
+    try:
+        file = open("binsnitch_data/alerts.log", 'r')
+    except IOError:
+        open("binsnitch_data/alerts.log", 'a').close()
+
+    refresh_cache()
+
+############
+#Entrypoint#
+############
+parser = argparse.ArgumentParser()
+parser.add_argument("dir", type=str, help="the directory to monitor")
+parser.add_argument("-v", "--verbose", action="store_true", help="increase output verbosity")
+parser.add_argument("-s", "--singlepass", action="store_true", help="do a single pass over all files")
+parser.add_argument("-a", "--all", action="store_true", help="keep track of all files, not only executables")
+parser.add_argument("-n", "--new", action="store_true", help="alert on new files too, not only on modified files")
+parser.add_argument("-b", "--baseline", action="store_true", help="do not generate alerts (useful to create baseline)")
+parser.add_argument("-w", "--wipe", action="store_true", help="start with a clean db.json and alerts.log file")
+
+args = parser.parse_args()
+prepare_data_files(args)
+
+logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt = '%m/%d/%Y %I:%M:%S %p',
+                    filename = "binsnitch_data/alerts.log",
+                    level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler())
+
+logging.info("binsnitch.py started")
+
+if not os.path.isdir(args.dir):
+    print("Error: " + args.dir + " could not be read, exiting.")
+    exit()
+
+print("Loaded " + str(len(cached_db)) + " items from db.json into cache")
+
+keepRunning = True
+while keepRunning:
+    logging.info("Scanning system for new and modified files, this can take a long time")
+
+    for dirName, subdirList, fileList in os.walk(args.dir, topdown=False):
+        try:
+            if args.verbose:
+                print('Scanning %s' % dirName)
+        except UnicodeEncodeError as e:
+            continue
+
+        for filename in fileList:
+            full_path = os.path.join(dirName, filename)
+            file_extension = str.upper(os.path.splitext(full_path)[1][1:])
+
+            try:
+                process_file = False
+
+                if args.all:
+                    process_file = True
+                else:
+                    if file_extension in dangerous_extensions:
+                        process_file = True
+
+                if process_file:
+                    file_hash = sha256_checksum(full_path)
+
+                    file_info = dict()
+                    file_info["path"] = full_path
+                    file_info["sha256"] = file_hash
+
+                    status = check_file_status(file_info)
+
+                    if status == FILE_UNKNOWN:
+                        add_file_to_db(file_info)
+
+                        if args.new and not args.baseline:
+                            add_alert_do_db(file_info, FILE_UNKNOWN)
+
+                    elif status == FILE_KNOWN_TOUCHED:
+                        if not args.baseline:
+                            add_alert_do_db(file_info, FILE_KNOWN_TOUCHED)
+
+                    elif status == FILE_KNOWN_UNTOUCHED:
+                        pass
+
+            except Exception as exc:
+                print(str(sys.exc_info()))
+
+    if not args.singlepass:
+        logging.info("Finished! Sleeping for a minute before scanning " + args.dir + " for changes again")
+        time.sleep(60)
+    else:
+        logging.info("Finished!")
+        keepRunning = False
